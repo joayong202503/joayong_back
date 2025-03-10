78f7bb4cf93576f685e8526b67fcf3d6ff0c2cbd
samchi <seyoung0314@gmail.com>
2025-03-10 21:25:58 +0900

    [feat] create custom repository for getRegionCategory

---
+++ src/main/java/com/joayong/skillswap/repository/custom/CategoryRegionRepositoryCustom.java
@@ -0,0 +1,9 @@
+package com.joayong.skillswap.repository.custom;
+
+import com.querydsl.core.Tuple;
+
+import java.util.List;
+
+public interface CategoryRegionRepositoryCustom {
+    public List<Tuple> getRegionCategory() ;
+}
