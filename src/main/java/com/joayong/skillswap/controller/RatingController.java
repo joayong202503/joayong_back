cb36f8313fd02f2bb63a4d507c374f2029505c4d
samchi <seyoung0314@gmail.com>
2025-03-14 15:04:27 +0900

    [feat] add getReview api (controller)

--- src/main/java/com/joayong/skillswap/controller/RatingController.java
+++ src/main/java/com/joayong/skillswap/controller/RatingController.java
@@ -1,15 +1,13 @@
 package com.joayong.skillswap.controller;
 
 import com.joayong.skillswap.domain.rating.dto.request.RatingRequest;
+import com.joayong.skillswap.domain.rating.dto.response.RatingResponse;
 import com.joayong.skillswap.service.RatingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -22,10 +20,19 @@ public class RatingController {
     public ResponseEntity<?> addReview(
             @RequestBody RatingRequest dto,
             @AuthenticationPrincipal String email
-    ){
-        log.info("email:{}",email);
-        log.info("dto : {}",dto);
+    ) {
+        log.info("email:{}", email);
+        log.info("dto : {}", dto);
         double v = ratingService.addRating(email, dto);
         return ResponseEntity.ok().body(v);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getReview(
+            @RequestParam String username
+    ) {
+        RatingResponse ratingList = ratingService.getRatingList(username);
+
+        return ResponseEntity.ok().body(ratingList);
     }
 }
