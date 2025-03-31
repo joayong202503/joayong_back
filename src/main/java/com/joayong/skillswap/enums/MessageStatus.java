fc79acf27fa3eced04ef4c6148af92ba4d7d465b
samchi <seyoung0314@gmail.com>
2025-03-31 16:34:48 +0900

    [feat] modify to allow mutual reviews

--- src/main/java/com/joayong/skillswap/enums/MessageStatus.java
+++ src/main/java/com/joayong/skillswap/enums/MessageStatus.java
@@ -3,7 +3,9 @@ package com.joayong.skillswap.enums;
 public enum MessageStatus {
     N, // 전송상태
     M, // 수락상태
-    C, // 최종 완료상태 (리뷰완료)
+    C, // 최종 완료상태 (리뷰 둘다 완료)
     D, // 거절 상태
-    R, // 완료 상태 (아직 리뷰안한 상태)
+    R, // 아직 리뷰 아무도 안단 상태
+    RS, // 메세지 보낸사람이 리뷰쓴 상태
+    RW, // 글쓴 사람이 리뷰쓴 상태
 }
